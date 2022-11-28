1f40d039b2112cf7ce3466b01ef34f0bf93508d4
roddyisthebest <bsy17171@naver.com>
2022-11-29 01:36:49 +0900

    [Level 2] Calendar 스크린 컴포넌트 기본구조 세팅

--- screen/Calendar.tsx
+++ screen/Calendar.tsx
@@ -1,10 +1,70 @@
-import {SafeAreaView, Text} from 'react-native';
+import {Platform} from 'react-native';
 import React from 'react';
-const Calendar = () => (
-  <SafeAreaView
-    style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-    <Text>Calendar</Text>
-  </SafeAreaView>
-);
+import styled from 'styled-components/native';
+import Icon from 'react-native-vector-icons/Ionicons';
+const ContainerWrapper = styled.SafeAreaView`
+  flex: 1;
+  background-color: white;
+`;
+
+const Header = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  height: 50px;
+`;
+const Container = styled.View<{
+  os: 'android' | 'ios' | 'windows' | 'macos' | 'web';
+}>`
+  padding: ${props => (props.os === 'android' ? '5px 20px' : '0 20px')};
+`;
+
+const HeaderTitle = styled.Text`
+  color: black;
+  font-size: 20px;
+  font-weight: 500;
+`;
+const Body = styled.View`
+  margin-top: 10px;
+`;
+const Day = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  height: 50px;
+  flex-wrap: wrap;
+  margin-bottom: 10px;
+`;
+
+const DayText = styled.Text`
+  color: rgb(129, 129, 132);
+  font-size: 15px;
+  font-weight: 300;
+`;
+
+const Calendar = () => {
+  return (
+    <ContainerWrapper>
+      <Container os={Platform.OS}>
+        <Header>
+          <Icon name={'chevron-back-outline'} color={'black'} size={20} />
+          <HeaderTitle>2022년 7월</HeaderTitle>
+          <Icon name={'chevron-forward-outline'} color={'black'} size={20} />
+        </Header>
+        <Body>
+          <Day>
+            <DayText>일</DayText>
+            <DayText>월</DayText>
+            <DayText>화</DayText>
+            <DayText>수</DayText>
+            <DayText>목</DayText>
+            <DayText>금</DayText>
+            <DayText>토</DayText>
+          </Day>
+        </Body>
+      </Container>
+    </ContainerWrapper>
+  );
+};
 
 export default Calendar;
